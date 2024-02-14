a0735556e8126a5b8f5543be40b6ab84cc9f676e
amitark7 <136192783+amitark7@users.noreply.github.com>
2024-02-14 16:46:26 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Signup from './component/Signup'
+
+const App = () => {
+  return (
+    <View>
+      <Signup/>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
