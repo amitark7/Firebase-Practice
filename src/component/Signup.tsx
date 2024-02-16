b24733a3421a2794ac6f0591c5c45c55ed597617
amitark7 <136192783+amitark7@users.noreply.github.com>
2024-02-16 20:02:09 +0530

    Update Signup.tsx

--- src/component/Signup.tsx
+++ src/component/Signup.tsx
@@ -21,7 +21,6 @@ const Signup = () => {
         console.log('User account created & signed in!');
       })
       .catch(error => {
-        
         if (error.code === 'auth/email-already-in-use') {
           console.log('That email address is already in use!');
         }
